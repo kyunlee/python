e51f92e7ff73503184f9550b99a40a2266ad731c
kyunlee <kyunlee87@gmail.com>
2024-01-23 13:01:52 +0900

    study0123

--- studypy03.py
+++ studypy03.py
@@ -25,3 +25,26 @@ a = ['a', 'c', 'b']
 a.reverse()
 print(a)
 
+a = [1, 2, 3]
+a.index(3)
+print(a)
+
+a = [1, 2, 3]
+a.insert(0,4)
+print(a)
+
+a = [1, 2, 3, 1, 2, 3]
+a.remove(3)   #값으로 삭제 
+print('remove',a)
+
+a = [1, 2, 3]
+a.pop()  #pop index로 삭제 값
+print('pop',a)
+
+a = [1, 2, 3, 1]
+a.count(1)
+print(a)
+
+a = [1, 2, 3]
+a.extend([4, 5])
+print(a)
