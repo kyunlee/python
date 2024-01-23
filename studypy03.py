4722deb03a840c396157bbd57385a6d7a759bc5a
kyunlee <kyunlee87@gmail.com>
2024-01-23 12:48:56 +0900

    study0123

---
+++ studypy03.py
@@ -0,0 +1,27 @@
+#리스트
+a = [1, 2, 3]
+a[2] = 4
+print(a)
+
+del a[1]
+print(a)
+
+a = [1, 2, 3, 4, 5]
+del a[2:]
+print(a)
+
+a = [1, 2, 3]
+a.append(4)
+print(a)
+
+a.append([5, 6])
+print(a)
+
+a = [1, 4, 3, 2]
+a.sort()
+print(a)
+
+a = ['a', 'c', 'b']
+a.reverse()
+print(a)
+
