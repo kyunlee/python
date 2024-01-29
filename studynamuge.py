c44a564d81cdb6c7f591227447788dc9a94ca380
kyunlee <kyunlee87@gmail.com>
2024-01-29 14:58:33 +0900

    namugestudy

---
+++ studynamuge.py
@@ -0,0 +1,11 @@
+#딕셔너리 자료형
+# 연관배열(asscociative array) 또는 '해시(hash) 라고 함.
+
+# 딕셔너리와 리스트나 튜플의 차이?
+# 순차적으로(sequential)해당 요솟값을 구하는지 않고 Key를 통해 Value를 얻음.
+# {Key1:Value1, Key2:Value2, Key3:Value3, ...}
+
+dic = {'name':'pey', 'phone': '010-9999-1234', 'birth':'1118'}
+print(dic)
+
+# 딕셔너리 쌍 추가하기
