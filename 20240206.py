db01264ed888e92ca95b948c6497b0c7b8e16e54
kyunlee <kyunlee87@gmail.com>
2024-02-06 12:15:58 +0900

    while

--- 20240206.py
+++ 20240206.py
@@ -50,3 +50,14 @@ while a < 10:
     a = a+1
     if a % 2 == 0: continue
     print(a)
+
+# 무한 루프
+'''
+while True:
+    수행할_문장1
+    수행할_문장2
+'''
+
+while True:
+    print("Ctrl+C를 눌러야 while문을 빠져나갈 수 있습니다.")
+    
