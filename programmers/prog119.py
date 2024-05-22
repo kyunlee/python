e6e21d1774a5c3edc22a1938e38a53f296e19807
kyunlee <kyunlee87@gmail.com>
2024-05-22 17:42:02 +0900

    프로그래머스

--- programmers/prog119.py
+++ programmers/prog119.py
@@ -1,17 +1,80 @@
 # 세개의 구분자
 
-myStr = "baconlettucetomato"
+myStr = "cabab"
 
 def solution(myStr):
-    return myStr
+    import re
+    return ["EMPTY"] if list(filter(None,re.split('[abc]',myStr))) == [] else list(filter(None,re.split('[abc]',myStr)))
 
-print(solution(myStr))
+#print(solution(myStr))
 
 # 배열의 원소만큼 추가하기
 
 arr= [5,1,4]
 
 def solution(arr):
-    return [str(i)*i for i in arr]
+    answer = []
+    for i in arr:
+        answer += [i]*i
+    return answer
+
+print(solution(arr))
+
+# 빈 배열에 추가, 삭제하기
+
+arr = [3,2,4,1,3]
+flag = [True, False, True, False, False]
+
+def solution(arr, flag):
+    answer = []
+    for i,j in enumerate(arr):
+        if flag[i]: 
+            answer += [j]*(j*2)
+        else:
+            for j in range(0,j):
+                answer.pop()
+    return answer
+
+#print(solution(arr,flag))
+
+# 배열만들기 6
+
+arr = [0, 1, 1, 1, 0]
+def solution(arr):
+    answer = []
+    i = 0
+    while i < len(arr):
+        if answer == []: 
+            answer.append(arr[i]) 
+            i+=1
+        elif answer[len(answer)-1] == arr[i]:
+            answer.pop()
+            i+=1
+        elif answer[len(answer)-1] != arr[i]:
+            answer.append(arr[i]) 
+            i+=1
+    return answer or [-1]
 
 #print(solution(arr))
+
+#무작위로 K개의 수 뽑기
+
+arr = [0, 1, 1]	
+k = 4
+
+def solution(arr, k):
+    answer = []
+    for i in arr:
+        if i not in answer:
+            answer.append(i)
+            print(answer)
+        if len(answer) >= k : return answer
+
+    for _ in range(len(answer),k):
+        answer.append(-1)
+    return answer
+
+           
+print(solution(arr, k))
+
+
--- programmers/prog219.py
+++ programmers/prog219.py
@@ -1,1 +1,43 @@
 # 7개의 개수
+
+array = [7,77,17]
+
+def solution(array):
+    return str(array).count(str('7'))
+        
+
+#print(solution(array))
+
+# 잘라서 뱅열로 저장하기
+
+my_str = "abc1Addfggg4556b"
+n = 6
+
+def solution(my_str, n):
+    return [my_str[i:i+n] for i in range(0,len(my_str),n)]
+
+#print(solution(my_str, n))
+
+# 중복된 숫자 개수
+
+array = [0, 10]
+n = 0
+
+ct = 0
+for i in array:
+    if i == n: ct +=1
+
+#print (ct)
+
+# 머쓱이보다 키 큰 사람
+array = [149, 180, 192, 170]
+height = 167
+
+def solution(array, height):
+    answer = 0
+    array.sort()
+    for i,j in enumerate(array):
+        if j > height: return len(array)- i
+    return answer
+
+print(solution(array, height))
